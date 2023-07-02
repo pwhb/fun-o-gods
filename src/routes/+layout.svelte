ec4c769c103294826331c35c5560f357e0cc683b
phonewaiyamhein <phonewaiyamhein@ayabank.com>
2023-07-02 16:33:24 +0630

    nav progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script>
+	import NavProgress from '$lib/components/common/nav_progress.svelte';
 	import '../app.css';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 
 	// Optionally set default options here
-	const options = {};
+	const options = { classes: ['log'] };
 </script>
 
 <main data-theme="cupcake" class="h-screen bg-base-300">
 	<slot />
 	<SvelteToast {options} />
+	<NavProgress />
 </main>
+
+<style>
+	:global(.log.info) {
+		--toastBackground: #36d399;
+	}
+	:global(.log.warn) {
+		--toastBackground: #f87272;
+	}
+</style>
